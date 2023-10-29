ed0c82f68d8cc6f253c4324d16020c91603a0fe5
HarryPotato986 <gdude986@gmail.com>
2023-10-28 20:43:42 -0400

    🐈

--- src/main/java/io/github/HarryPotato986/Gases_and_Wormholes/init/datagen/ModBlockTagGenerator.java
+++ src/main/java/io/github/HarryPotato986/Gases_and_Wormholes/init/datagen/ModBlockTagGenerator.java
@@ -1,8 +1,10 @@
 package io.github.HarryPotato986.Gases_and_Wormholes.init.datagen;
 
 import io.github.HarryPotato986.Gases_and_Wormholes.Gases_and_Wormholes;
+import io.github.HarryPotato986.Gases_and_Wormholes.init.block.BlockInit;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -16,7 +18,11 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(BlockInit.TEMP_BLOCK.get());
 
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(BlockInit.TEMP_BLOCK.get());
     }
 
     @Override
